04f83547689a004458cf7c07fdd15ec52d7b7f79
lazaromanuelguerrasgutierrez-ux <lazaromanuelguerrasgutierrez@gmail.com>
2026-03-30 00:04:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 8080;
 app.get("/", (req, res) => {
   res.send("Bot activo 🚀");
 });
